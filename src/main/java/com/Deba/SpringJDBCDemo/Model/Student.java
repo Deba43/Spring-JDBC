5578fa3dd705afb5b615e7460b3fce0b34d445d6
Deba43 <debadatta016@gmail.com>
2024-11-14 11:07:48 +0530

    adding Lombok to not use getter and setter

--- src/main/java/com/Deba/SpringJDBCDemo/Model/Student.java
+++ src/main/java/com/Deba/SpringJDBCDemo/Model/Student.java
@@ -2,46 +2,16 @@ package com.Deba.SpringJDBCDemo.Model;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import lombok.Data;
 
 @Component
 @Scope("prototype")
+@Data
 public class Student {
     private int id;
     private int age;
     private String name;
     private String tech;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTech() {
-        return tech;
-    }
-
-    public void setTech(String tech) {
-        this.tech = tech;
-    }
 
     @Override
     public String toString() {
--- src/main/java/com/Deba/SpringJDBCDemo/repo/DebaRepo.java
+++ src/main/java/com/Deba/SpringJDBCDemo/repo/DebaRepo.java
@@ -3,45 +3,45 @@ package com.Deba.SpringJDBCDemo.repo;
 import com.Deba.SpringJDBCDemo.Model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-@Component
 
+@Component
 public class DebaRepo {
     private JdbcTemplate template;
 
     public JdbcTemplate getTemplate() {
         return template;
     }
+
     @Autowired
 
     public void setTemplate(JdbcTemplate template) {
         this.template = template;
     }
 
-    public void save(Student s){
+    public void save(Student s) {
         String sql = "insert into Student(id, age, name, tech) values(?,?,?,?)";
         int rows = template.update(sql, s.getId(), s.getAge(), s.getName(), s.getTech());
         System.out.println(rows + "rows affected");
 
     }
 
-    public List<Student> findAll(){
+    public List<Student> findAll() {
 
         String sql = "Select * from Student";
 
-        List<Student> s2 = template.query(sql,(rs, row) ->  {
+        List<Student> s2 = template.query(sql, (rs, row) -> {
 
-                Student stud = new Student();
+            Student stud = new Student();
 
-                stud.setId(rs.getInt(1));
-                stud.setAge(rs.getInt(2));
-                stud.setName(rs.getString(3));
-                stud.setTech(rs.getString(4));
+            stud.setId(rs.getInt(1));
+            stud.setAge(rs.getInt(2));
+            stud.setName(rs.getString(3));
+            stud.setTech(rs.getString(4));
 
-                return stud;
+            return stud;
 
         });
 
